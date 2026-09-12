8013d528b2f8cab325dafae200d8d8aa16be720f
rodrigoshez <sanchezcoronarodrigo1@gmail.com>
2026-09-12 13:15:44 -0600

    feat: agrega scopes de gmail al flujo de autorizacion de google

--- src/calendar.ts
+++ src/calendar.ts
@@ -70,6 +70,8 @@ export function getGoogleAuthUrl() {
     scope: [
       "https://www.googleapis.com/auth/calendar.events",
       "https://www.googleapis.com/auth/drive.metadata.readonly",
+      "https://www.googleapis.com/auth/gmail.readonly",
+      "https://www.googleapis.com/auth/gmail.send",
     ],
   });
 }
